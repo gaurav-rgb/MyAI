7c7261235f0e08b400c1a63b4c3c7f951bacfe35
gaurav-rgb <gauravmaurya8972@gmail.com>
2024-07-28 17:03:25 +0530

    Update build

--- backend/index.js
+++ backend/index.js
@@ -161,10 +161,10 @@ app.use((err, req, res, next) => {
 });
 
 // PRODUCTION
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../client")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client", "index.html"));
 });
 
 app.listen(port, () => {
